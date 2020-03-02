048480220cb590f7e9f7f9341a56d0a7b1cf863a
Christoph Heuwieser <dropdead@hashcat.net>
2020-03-02 19:25:07 +0100

    Update entrypointwrapper.sh

--- entrypointwrapper.sh
+++ entrypointwrapper.sh
@@ -5,7 +5,8 @@ echo ip=$IP
 
 consul agent -bind=$IP -retry-join=$CONSUL_URL -data-dir /opt/consul-data  -config-dir /etc/consul.d &
 
-filebeat -e -c /etc/filebeat/filebeat.yml -path.home /usr/share/filebeat -path.config /etc/filebeat -path.data /var/lib/filebeat -path.logs /var/log/filebeat &
+# disabled because of lots of faulty logs that can't be parsed
+#filebeat -e -c /etc/filebeat/filebeat.yml -path.home /usr/share/filebeat -path.config /etc/filebeat -path.data /var/lib/filebeat -path.logs /var/log/filebeat &
 
 
 # back to the real entrypoint - shouldn't be needed for kafka since dockerfile supports custom init sript
